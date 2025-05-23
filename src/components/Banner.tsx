aed1c379c34f3ce5382fee8c1284c76c79bab1c4
Virendra Patil <virendrapatil.24vp@gmail.com>
2025-05-23 16:47:53 +0530

    fixs banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="py-3 text-center bg-[linear-gradient(to_right,rgba(252,_214,_255,_0.7),rgba(41,_216,_255,_0.7),rgba(255,_253,_128,_0.7),rgba(248,_154,_192,_0.7),rgba(252,_214,_255,_0.7))]">
       <div className="container">
-        <p className="font-semibold">
+        <p className="font-medium">
           <span className="hidden sm:inline">
             Introducing a completely redesigned interface —{" "}
           </span>
