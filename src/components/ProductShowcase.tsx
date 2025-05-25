69c8cbb9fe966e2cf2b20fda782521cf12a0d2e3
Virendra Patil <virendrapatil.24vp@gmail.com>
2025-05-25 15:43:50 +0530

    adds project showcase section

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Image from "next/image";
+import ProductImage from "../assets/images/app-screen.png";
 
 const ProductShowcase = () => {
-  return <div>ProductShowcase</div>;
+  return (
+    <div className="text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-5xl sm:text-6xl text-center font-bold">
+          Intuitive interface
+        </h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-xl mt-5 text-center text-white/70">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes,
+            one task at a time.
+          </p>
+        </div>
+        <Image
+          src={ProductImage}
+          alt="Product Showcase"
+          className="mt-14 mx-auto"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProductShowcase;
