91ba9b4b193f8655e3b2e2d96b4caa7435fce0e2
Virendra Patil <virendrapatil.24vp@gmail.com>
2025-05-25 17:46:04 +0530

    adds CTA  section

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import helixImage from "../assets/images/helix2.png";
+import emojiStarImage from "../assets/images/emojistar.png";
+import Image from "next/image";
 
 const CallToAction = () => {
-  return <div>CallToAction</div>;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container max-w-xl relative">
+        <Image
+          src={helixImage}
+          alt="helix-image"
+          className="hidden sm:inline absolute top-6 left-[calc(100%+36px)]"
+        />
+        <Image
+          src={emojiStarImage}
+          alt="emoji-star"
+          className="hidden sm:inline absolute -top-[120px] right-[calc(100%+24px)]"
+        />
+        <h2 className="font-bold text-center text-5xl sm:text-6xl">
+          Get instant access
+        </h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-xl text-white/70 mt-5 text-center">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+        </div>
+        <div className="mt-10 flex flex-col sm:flex-row max-w-sm mx-auto gap-2.5">
+          <input
+            type="email"
+            placeholder="your@email.com"
+            className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"
+          />
+          <button className="bg-white text-black h-12 rounded-lg px-5">
+            Get access
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CallToAction;
