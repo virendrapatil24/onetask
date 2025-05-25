3d69da87be9a6ad2343257a545be09f74117056d
Virendra Patil <virendrapatil.24vp@gmail.com>
2025-05-25 19:24:41 +0530

    adds footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import InstaIcon from "../assets/icons/insta.svg";
+import XIcon from "../assets/icons/x-social.svg";
+import LinkedInIcon from "../assets/icons/linkedin.svg";
+import GithubIcon from "../assets/icons/github.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+      <div className="container">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
+          <div className="text-center items-center">
+            © 2025 Developed with ❤️ by{" "}
+            <a
+              href="https://www.virendrapatil.xyz/"
+              target="_blank"
+              className="font-bold text-white/80 hover:text-white"
+            >
+              Virendra Patil
+            </a>
+          </div>
+          <ul className="flex gap-2.5">
+            <li>
+              <a href="https://github.com/virendrapatil24" target="_blank">
+                <GithubIcon />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/virendrapatil_4/"
+                target="_blank"
+              >
+                <InstaIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/virendrapatil24" target="_blank">
+                <XIcon />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/virendrapatil24/"
+                target="_blank"
+              >
+                <LinkedInIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
