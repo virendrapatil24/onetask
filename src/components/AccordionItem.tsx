e8c33b18942d262a908b1ac3f3ca7709f88e6954
Virendra Patil <virendrapatil.24vp@gmail.com>
2025-05-25 21:07:11 +0530

    adds animation to FAQs section

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -4,6 +4,7 @@ import PlusIcon from "../assets/icons/plus.svg";
 import MinusIcon from "../assets/icons/minus.svg";
 import { useState } from "react";
 import clsx from "clsx";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface AccordionItemProps {
   question: string;
@@ -22,7 +23,29 @@ const AccordionItem = ({ question, answer }: AccordionItemProps) => {
         <span className="flex-1 text-lg font-bold">{question}</span>
         {isOpen ? <MinusIcon /> : <PlusIcon />}
       </div>
-      <div className={clsx("mt-4", { hidden: !isOpen })}>{answer}</div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0,
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              marginTop: "16px",
+            }}
+            exit={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0,
+            }}
+          >
+            {answer}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
